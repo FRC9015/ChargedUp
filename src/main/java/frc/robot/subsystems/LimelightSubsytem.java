139b825ea02c277e97f7109677705b40b9fd6623
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-11 10:12:42 -0500

    rough limelight code

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsytem.java
@@ -0,0 +1,69 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSubsytem extends SubsystemBase {
+
+    NetworkTable limelight;
+    NetworkTableEntry tx, ty, ta, tv, tid;
+
+    public static enum CamMode {
+        VISION, // Vision processor
+        DRIVER  // Driver Camera (Increases exposure, disables vision processing)
+    }
+
+    public LimelightSubsytem() {
+        limelight = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = limelight.getEntry("tx");
+        ty = limelight.getEntry("ty");
+        ta = limelight.getEntry("ta");
+        tv = limelight.getEntry("tv");
+        tid = limelight.getEntry("tid");
+    }
+
+    /* 
+     * Check if the camera has (a) target(s) in sight
+     */
+    public boolean hasTargets() {
+        double tvResult = tv.getDouble(0);
+        if (tvResult == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /* 
+     * Get the ID of the detected primary AprilTag
+     */
+    public int getPrimaryAprilTag() {
+        double rawTagId = tid.getDouble(0);
+        return (int) rawTagId;
+    }
+
+    /* 
+     * Set the mode of the camera
+     */
+    public void setMode(CamMode newMode) {
+        NetworkTableEntry numMode = limelight.getEntry("camMode");
+        if (newMode == CamMode.VISION) {
+            numMode.setNumber(0);
+        } else if (newMode == CamMode.DRIVER) {
+            numMode.setNumber(1);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+
+    }
+
+}
