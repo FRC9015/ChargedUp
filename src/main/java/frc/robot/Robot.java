0b2806815f29071c83e003736c96dae71516a8b4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-17 09:15:57 -0500

    allow the command scheduler to run in test mode

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -114,7 +114,7 @@ public class Robot extends TimedRobot
     public void testInit()
     {
         // Cancels all running commands at the start of test mode.
-        CommandScheduler.getInstance().cancelAll();
+        // CommandScheduler.getInstance().cancelAll();
     }
     
     
