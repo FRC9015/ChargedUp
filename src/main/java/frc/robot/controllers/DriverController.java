869fe9922f42e526faed55414aa260b681f360ad
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-18 13:34:34 -0500

    add balance button

--- src/main/java/frc/robot/controllers/DriverController.java
+++ src/main/java/frc/robot/controllers/DriverController.java
@@ -7,16 +7,20 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class DriverController implements Sendable {
 
     private final DoubleSupplier tankLeft, tankRight, arcadeFwd, arcadeTurn;
+    private final JoystickButton balanceButton;
 
-    /** Create a driver controller with a standard Joystick */
+    /** Create a driver controller with two standard Joysticks */
     public DriverController(Joystick joystick1, Joystick joystick2) {
         tankLeft = joystick1::getY;
         tankRight = joystick2::getY;
         arcadeFwd = joystick1::getY;
         arcadeTurn = joystick1::getX;
+
+        balanceButton = null;
     }
 
     /** Create a driver controller with an XboxController */
@@ -25,6 +29,8 @@ public class DriverController implements Sendable {
         tankRight = controller::getRightY;
         arcadeFwd = controller::getLeftY;
         arcadeTurn = controller::getRightX;
+
+        balanceButton = new JoystickButton(controller, XboxController.Button.kA.value);
     }
 
     /** Create a driver controller with a PS4Controller */
@@ -34,6 +40,7 @@ public class DriverController implements Sendable {
         arcadeFwd = controller::getLeftY;
         arcadeTurn = controller::getRightX;
 
+        balanceButton = new JoystickButton(controller, PS4Controller.Button.kTriangle.value);
     }
 
     public double getTankLeft() {
@@ -51,6 +58,10 @@ public class DriverController implements Sendable {
     public double getArcadeTurn() {
         return arcadeTurn.getAsDouble();
     }
+
+    public JoystickButton getBalanceButton() {
+        return balanceButton;
+    }
     
     // This allows us to send this DriverController to the Dashboard and read all of its values
     @Override
@@ -60,6 +71,7 @@ public class DriverController implements Sendable {
         builder.addDoubleProperty("arcadeTurn", arcadeTurn, null);
         builder.addDoubleProperty("tankLeft", tankLeft, null);
         builder.addDoubleProperty("tankLeft", tankRight, null);
+        builder.addBooleanProperty("balanceButton", balanceButton::getAsBoolean, null);
     }
 
 }
