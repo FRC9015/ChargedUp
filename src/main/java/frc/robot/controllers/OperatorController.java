07b87304ea30471b9420b819cc0de1b8934d04ba
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-18 13:05:55 -0500

    implement Sendable in OperatorController

--- src/main/java/frc/robot/controllers/OperatorController.java
+++ src/main/java/frc/robot/controllers/OperatorController.java
@@ -1,9 +1,11 @@
 package frc.robot.controllers;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class OperatorController {
+public class OperatorController implements Sendable {
 
     /** Create an operator controller with an XboxController */
     public OperatorController(XboxController controller) {
@@ -13,6 +15,12 @@ public class OperatorController {
     /** Create an operator controller with a PS4Controller */
     public OperatorController(PS4Controller controller) {
 
+    }
+
+    // This allows us to send this OperatorController to the Dashboard and read all of its values
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("OperatorController");
     }
 
 }
