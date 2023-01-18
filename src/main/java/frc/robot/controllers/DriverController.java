9ad643e49ef02c976e6b9cde02186fd938b93563
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-18 14:18:40 -0500

    Finally implement Abstract Controllers (#1)

--- src/main/java/frc/robot/controllers/DriverController.java
+++ src/main/java/frc/robot/controllers/DriverController.java
@@ -1,24 +1,95 @@
 package frc.robot.controllers;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+public class DriverController implements Sendable {
 
-public class DriverController {
+    private final DoubleSupplier tankLeft, tankRight, arcadeFwd, arcadeTurn;
+    private final JoystickButton balanceButton, switchSpeed;
+    private final GenericHID rawController;
 
-    /** Create a driver controller with a standard Joystick */
-    public DriverController(Joystick joystick) {
+    /** Create a driver controller with two standard Joysticks */
+    public DriverController(Joystick joystick1, Joystick joystick2) {
+        tankLeft = joystick1::getY;
+        tankRight = joystick2::getY;
+        arcadeFwd = joystick1::getY;
+        arcadeTurn = joystick1::getX;
 
+        balanceButton = null;
+        switchSpeed = new JoystickButton(joystick1, Joystick.ButtonType.kTrigger.value);
+        rawController = joystick1;
     }
 
     /** Create a driver controller with an XboxController */
     public DriverController(XboxController controller) {
+        tankLeft = controller::getLeftY;
+        tankRight = controller::getRightY;
+        arcadeFwd = controller::getLeftY;
+        arcadeTurn = controller::getRightX;
 
+        balanceButton = new JoystickButton(controller, XboxController.Button.kA.value);
+        switchSpeed = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
+
+        rawController = controller;
     }
 
     /** Create a driver controller with a PS4Controller */
     public DriverController(PS4Controller controller) {
+        tankLeft = controller::getLeftY;
+        tankRight = controller::getRightY;
+        arcadeFwd = controller::getLeftY;
+        arcadeTurn = controller::getRightX;
 
+        balanceButton = new JoystickButton(controller, PS4Controller.Button.kTriangle.value);
+        switchSpeed = new JoystickButton(controller, PS4Controller.Button.kL3.value); // Left Joystick button
+
+        rawController = controller;
+    }
+
+    public double getTankLeft() {
+        return tankLeft.getAsDouble();
+    }
+
+    public double getTankRight() {
+        return tankRight.getAsDouble();
+    }
+
+    public double getArcadeFwd() {
+        return arcadeFwd.getAsDouble();
+    }
+
+    public double getArcadeTurn() {
+        return arcadeTurn.getAsDouble();
+    }
+
+    public JoystickButton getBalanceButton() {
+        return balanceButton;
+    }
+
+    public JoystickButton getSwitchSpeed() {
+        return switchSpeed;
+    }
+
+    public void setRumble(GenericHID.RumbleType rumble, double value) {
+        rawController.setRumble(rumble, value);
+    }
+    
+    // This allows us to send this DriverController to the Dashboard and read all of its values
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("DriverController");
+        builder.addDoubleProperty("arcadeFwd", arcadeFwd, null);
+        builder.addDoubleProperty("arcadeTurn", arcadeTurn, null);
+        builder.addDoubleProperty("tankLeft", tankLeft, null);
+        builder.addDoubleProperty("tankLeft", tankRight, null);
+        builder.addBooleanProperty("balanceButton", balanceButton::getAsBoolean, null);
     }
 
 }
--- src/main/java/frc/robot/controllers/OperatorController.java
+++ src/main/java/frc/robot/controllers/OperatorController.java
@@ -1,18 +1,33 @@
 package frc.robot.controllers;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class OperatorController {
+public class OperatorController implements Sendable {
+
+    private final GenericHID rawController;
 
     /** Create an operator controller with an XboxController */
     public OperatorController(XboxController controller) {
-
+        rawController = controller;
     }
 
     /** Create an operator controller with a PS4Controller */
     public OperatorController(PS4Controller controller) {
+        rawController = controller;
+    }
 
+    public void setRumble(GenericHID.RumbleType rumble, double value) {
+        rawController.setRumble(rumble, value);
+    }
+
+    // This allows us to send this OperatorController to the Dashboard and read all of its values
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("OperatorController");
     }
 
 }
