6b1c8561356a4c8ffe7c1bd814368ed427cc68e0
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-01-26 08:49:17 -0500

    new AutoPaths singleton class handles basic functionality for reading and using PathPlanner trajectories

---
+++ src/main/java/frc/robot/AutoPaths.java
@@ -0,0 +1,61 @@
+package frc.robot;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.Constants.DriveConstants;
+
+public class AutoPaths {
+    private static final AutoPaths INSTANCE = new AutoPaths();
+
+    public static AutoPaths getInstance() {
+        return INSTANCE;
+    }
+
+    private File pathsDir;
+    private File[] pathFiles;
+    private ArrayList<String> pathNames;
+    private SendableChooser<PathPlannerTrajectory> paths;
+
+    private AutoPaths() {
+        pathsDir = new File(Filesystem.getDeployDirectory(), "pathplanner");
+
+        pathNames = new ArrayList<String>();
+        paths = new SendableChooser<PathPlannerTrajectory>();
+    }
+
+    public void init() {
+        pathFiles = pathsDir.listFiles();
+
+        for (File pathFile : pathFiles) {
+            String pathFileName = pathFile.getName();
+
+            if (pathFileName.indexOf(".path") >= 0) {
+                String pathName = pathFileName.substring(0, pathFileName.indexOf(".path"));
+
+                pathNames.add(pathName);
+            }
+        }
+
+        for (String pathName: pathNames) {
+            PathPlannerTrajectory traj = PathPlanner.loadPath(pathName, DriveConstants.kPathConstraints);
+
+            paths.addOption(pathName, traj);
+        }
+    }
+
+    public String[] getPathNames() {
+        String[] constantPathNames = new String[pathNames.size()];
+
+        return pathNames.toArray(constantPathNames);
+    }
+
+    public SendableChooser<PathPlannerTrajectory> getChooser() {
+        return paths;
+    }
+}
