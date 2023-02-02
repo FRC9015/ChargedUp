ef0f262f3c1205e1de90a3890172d0c1f3b52525
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-02-02 17:59:33 -0500

    clean up AutoPaths.java

--- src/main/java/frc/robot/AutoPaths.java
+++ src/main/java/frc/robot/AutoPaths.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DriveConstants;
 
 public class AutoPaths {
