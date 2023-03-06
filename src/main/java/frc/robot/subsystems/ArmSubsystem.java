725f58b9bb42a1ec42980cb985998424c039f848
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-06 10:34:44 -0500

    create encoders & PID for rotate + telescope

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,13 +1,14 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants.IntakeConstants;
+import frc.robot.utils.PIDFConstants;
 public class ArmSubsystem implements Subsystem
 {
     private final static ArmSubsystem INSTANCE = new ArmSubsystem();
@@ -18,12 +19,18 @@ public class ArmSubsystem implements Subsystem
     }
 
 
-    private final CANSparkMax rotateArm, rotateArmSecond; //rotateArm pivots the arm. 
-    private final CANSparkMax telescopeArm; //telescopeArm moves the arm in and out.
+    private final CANSparkMax rotateArm, rotateArmSecond; // rotateArm pivots the arm. 
+    private final RelativeEncoder rotateEncoder;
+    private final SparkMaxPIDController rotatePID;
+    private final PIDFConstants rotatePIDConstants;
 
     private final DoubleSolenoid rotateArmBrake;
 
-    // Creates a new ArmSubsystem.
+    private final CANSparkMax telescopeArm; // telescopeArm moves the arm in and out.
+    private final RelativeEncoder telescopeEncoder;
+    private final SparkMaxPIDController telescopePID;
+    private final PIDFConstants telescopePIDConstants;
+
     private ArmSubsystem() 
     {
         rotateArm = new CANSparkMax(IntakeConstants.ARM_LIFT_CAN_ID, MotorType.kBrushless);
@@ -31,9 +38,22 @@ public class ArmSubsystem implements Subsystem
 
         rotateArmSecond.follow(rotateArm);
 
+        rotateEncoder = rotateArm.getEncoder();
+        rotatePID = rotateArm.getPIDController();
+
+        rotatePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1);
+        rotatePIDConstants.updateSparkMax(rotatePID);
+
         rotateArmBrake = PneumaticHubSubsystem.getInstance().getDoubleSolenoid(IntakeConstants.ARM_BRAKE_SOLENOID);
+        rotateArmBrake.set(DoubleSolenoid.Value.kReverse);
 
         telescopeArm = new CANSparkMax(IntakeConstants.ARM_TELESCOPE_CAN_ID, MotorType.kBrushless);
+
+        telescopeEncoder = telescopeArm.getEncoder();
+        telescopePID = telescopeArm.getPIDController();
+
+        telescopePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1);
+        telescopePIDConstants.updateSparkMax(telescopePID);
     }
 
     public void rotateArm(double motorspeed){
