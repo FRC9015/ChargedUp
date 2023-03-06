ff96c7de4bb78f647876d1c56fa48285e64bf526
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-06 10:42:35 -0500

    use SubsystemBase, organize, and private helper methods

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,13 +3,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.utils.PIDFConstants;
-public class ArmSubsystem implements Subsystem
+public class ArmSubsystem extends SubsystemBase
 {
     private final static ArmSubsystem INSTANCE = new ArmSubsystem();
     
@@ -33,6 +34,7 @@ public class ArmSubsystem implements Subsystem
 
     private ArmSubsystem() 
     {
+        // Set up stuff for the Arm rotating motors
         rotateArm = new CANSparkMax(IntakeConstants.ARM_LIFT_CAN_ID, MotorType.kBrushless);
         rotateArmSecond = new CANSparkMax(IntakeConstants.ARM_LIFT_2_CAN_ID, MotorType.kBrushless);
 
@@ -41,19 +43,33 @@ public class ArmSubsystem implements Subsystem
         rotateEncoder = rotateArm.getEncoder();
         rotatePID = rotateArm.getPIDController();
 
-        rotatePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1);
+        rotatePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1); // THESE CONSTANTS MUST BE TUNED
         rotatePIDConstants.updateSparkMax(rotatePID);
 
+        // Initialize arm rotation brake and make sure it is released on boot
         rotateArmBrake = PneumaticHubSubsystem.getInstance().getDoubleSolenoid(IntakeConstants.ARM_BRAKE_SOLENOID);
         rotateArmBrake.set(DoubleSolenoid.Value.kReverse);
 
+        // Set up stuff for the Arm Telescoping motor
         telescopeArm = new CANSparkMax(IntakeConstants.ARM_TELESCOPE_CAN_ID, MotorType.kBrushless);
 
         telescopeEncoder = telescopeArm.getEncoder();
         telescopePID = telescopeArm.getPIDController();
 
-        telescopePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1);
+        telescopePIDConstants = new PIDFConstants(0.1, 0, 0 , 0, 0.1); // THESE CONSTANTS MUST BE TUNED
         telescopePIDConstants.updateSparkMax(telescopePID);
+    }
+
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+    }
+    
+    @Override
+    public void simulationPeriodic()
+    {
+        // This method will be called once per scheduler run during simulation
     }
 
     public void rotateArm(double motorspeed){
@@ -72,14 +88,20 @@ public class ArmSubsystem implements Subsystem
         return rotateArmBrake.get() == DoubleSolenoid.Value.kForward;
     }
 
-    public void periodic()
-    {
-        // This method will be called once per scheduler run
+
+    // ----------------- PRIVATE HELPER METHODS ----------------- //
+
+    private void applyBrake() {
+        double currentVel = rotateEncoder.getVelocity();
+
+        if (Math.abs(currentVel) < 0.01) {
+            rotateArmBrake.set(DoubleSolenoid.Value.kForward);
+            rotateArm.setIdleMode(IdleMode.kCoast);
+        }
     }
-    
-    
-    public void simulationPeriodic()
-    {
-        // This method will be called once per scheduler run during simulation
+
+    private void releaseBrake() {
+        rotateArm.setIdleMode(IdleMode.kBrake);
+        rotateArmBrake.set(DoubleSolenoid.Value.kReverse);
     }
 }
