a94c4b3b305ceea2b398a55a1fd3ae33c8a03d29
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-20 18:09:46 -0400

    merge in operator controller

--- src/main/java/frc/robot/controllers/OperatorController.java
+++ src/main/java/frc/robot/controllers/OperatorController.java
@@ -1,33 +1,88 @@
 package frc.robot.controllers;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import lombok.Getter;
 
 public class OperatorController implements Sendable {
 
     private final GenericHID rawController;
 
-    /** Create an operator controller with an XboxController */
+    private final DoubleSupplier joyLeftX, joyLeftY, joyRightX, joyRightY, leftTriggerValue, rightTriggerValue;
+    @Getter
+    private final JoystickButton A, B, X, Y, start, back, leftBumper, rightBumper, leftTrigger, rightTrigger;
+    @Getter
+    private final POVButton povUp, povDown, povLeft, povRight;
+
     public OperatorController(XboxController controller) {
+        joyLeftY = controller::getLeftY;
+        joyLeftX = controller::getLeftX;
+        joyRightY = controller::getRightY;
+        joyRightX = controller::getRightX;
+
+        leftTriggerValue = controller::getLeftTriggerAxis;
+        rightTriggerValue = controller::getRightTriggerAxis;
+
+        povUp = new POVButton(controller, 0);
+        povRight = new POVButton(controller, 90);
+        povDown = new POVButton(controller, 180);
+        povLeft = new POVButton(controller, 270);
+
+        A = new JoystickButton(controller, XboxController.Button.kA.value);
+        B = new JoystickButton(controller, XboxController.Button.kB.value);
+        X = new JoystickButton(controller, XboxController.Button.kX.value);
+        Y = new JoystickButton(controller, XboxController.Button.kY.value);
+
+        leftBumper = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
+        rightBumper = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
+
+        start = new JoystickButton(controller, XboxController.Button.kStart.value);
+        back = new JoystickButton(controller, XboxController.Button.kBack.value);
+        
+        leftTrigger = new JoystickButton(controller, XboxController.Axis.kLeftTrigger.value);
+        rightTrigger = new JoystickButton(controller, XboxController.Axis.kRightTrigger.value);
+
+
         rawController = controller;
     }
 
-    /** Create an operator controller with a PS4Controller */
-    public OperatorController(PS4Controller controller) {
-        rawController = controller;
+    public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("Operator");
+        
+    }
+
+    public double getLeftX() {
+        return joyLeftX.getAsDouble();
+    }
+
+    public double getLeftY() {
+        return joyLeftY.getAsDouble();
+    }
+
+    public double getRightX() {
+        return joyRightX.getAsDouble();
+    }
+
+    public double getRightY() {
+        return joyRightY.getAsDouble();
+    }
+
+    public Trigger getLTriggerAsButton(){
+        return new Trigger(()-> leftTriggerValue.getAsDouble() > 0.3);
+    }
+
+    public Trigger getRTriggerAsButton(){
+        return new Trigger(()-> rightTriggerValue.getAsDouble() > 0.3);
     }
 
     public void setRumble(GenericHID.RumbleType rumble, double value) {
         rawController.setRumble(rumble, value);
     }
-
-    // This allows us to send this OperatorController to the Dashboard and read all of its values
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        builder.setSmartDashboardType("OperatorController");
-    }
-
 }
