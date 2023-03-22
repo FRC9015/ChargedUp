bc0660964e11d9c66b235427357b0cec5d14f998
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-22 13:29:10 -0400

    proto joystick controls for bella

--- src/main/java/frc/robot/controllers/OperatorController.java
+++ src/main/java/frc/robot/controllers/OperatorController.java
@@ -5,6 +5,7 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
@@ -15,11 +16,11 @@ public class OperatorController implements Sendable {
 
     private final GenericHID rawController;
 
-    private final DoubleSupplier joyLeftX, joyLeftY, joyRightX, joyRightY, leftTriggerValue, rightTriggerValue;
+    private DoubleSupplier joyLeftX, joyLeftY, joyRightX, joyRightY, leftTriggerValue, rightTriggerValue;
     @Getter
-    private final JoystickButton A, B, X, Y, start, back, leftBumper, rightBumper, leftTrigger, rightTrigger;
+    private JoystickButton A, B, X, Y, start, back, leftBumper, rightBumper, leftTrigger, rightTrigger;
     @Getter
-    private final POVButton povUp, povDown, povLeft, povRight;
+    private POVButton povUp, povDown, povLeft, povRight;
 
     public OperatorController(XboxController controller) {
         joyLeftY = controller::getLeftY;
@@ -49,6 +50,23 @@ public class OperatorController implements Sendable {
         leftTrigger = new JoystickButton(controller, XboxController.Axis.kLeftTrigger.value);
         rightTrigger = new JoystickButton(controller, XboxController.Axis.kRightTrigger.value);
 
+
+        rawController = controller;
+    }
+
+    // Quick mockup of Joystick controls for Bella
+    public OperatorController(Joystick controller) {
+        joyLeftY = controller::getY;
+        joyLeftX = controller::getX;
+        joyRightY = controller::getY;
+        joyRightX = controller::getX;
+
+        povUp = new POVButton(controller, 0);
+        povRight = new POVButton(controller, 90);
+        povDown = new POVButton(controller, 180);
+        povLeft = new POVButton(controller, 270);
+
+        leftTriggerValue = () -> controller.getTriggerPressed() ? 1.0 : 0.0;
 
         rawController = controller;
     }
