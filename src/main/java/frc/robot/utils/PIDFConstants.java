067c57ff9cf42d9cafb76810107fd57120813456
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-29 13:55:36 -0400

    new update methods for PIDFConstants

--- src/main/java/frc/robot/utils/PIDFConstants.java
+++ src/main/java/frc/robot/utils/PIDFConstants.java
@@ -2,11 +2,14 @@ package frc.robot.utils;
 
 import com.revrobotics.SparkMaxPIDController;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 
 /**
- * Class that represents PIDF constants, and allows editing of them via Sendable.
+ * Class that represents PIDF constants, and allows editing of them via
+ * Sendable.
  */
 public class PIDFConstants implements Sendable {
     private double kP, kI, kD, kIZone, kFF;
@@ -14,6 +17,7 @@ public class PIDFConstants implements Sendable {
 
     /**
      * Construct with only basic PID constants
+     * 
      * @param P Proportional term
      * @param I Integral term
      * @param D Derivative term
@@ -24,6 +28,7 @@ public class PIDFConstants implements Sendable {
 
     /**
      * Construct with only basic PID constants
+     * 
      * @param P Proportional term
      * @param I Integral term
      * @param D Derivative term
@@ -37,24 +42,30 @@ public class PIDFConstants implements Sendable {
 
     /**
      * Construct constants for a PID controller with FeedFoward
-     * @param P Proportional term
-     * @param I Integral term
-     * @param D Derivative term
-     * @param IZone Specifies the range the |error| must be within for the integral term to take effect. Must be positive, or set to zero to disable.
-     * @param FF FeedForward term
+     * 
+     * @param P     Proportional term
+     * @param I     Integral term
+     * @param D     Derivative term
+     * @param IZone Specifies the range the |error| must be within for the integral
+     *              term to take effect. Must be positive, or set to zero to
+     *              disable.
+     * @param FF    FeedForward term
      */
     public PIDFConstants(double P, double I, double D, double IZone, double FF) {
         this(P, I, D, IZone, FF, null);
     }
 
-        /**
+    /**
      * Construct constants for a PID controller with FeedFoward
-     * @param P Proportional term
-     * @param I Integral term
-     * @param D Derivative term
-     * @param IZone Specifies the range the |error| must be within for the integral term to take effect. Must be positive, or set to zero to disable.
-     * @param FF FeedForward term
-     * @param name Name that should be shown on the dashboard
+     * 
+     * @param P     Proportional term
+     * @param I     Integral term
+     * @param D     Derivative term
+     * @param IZone Specifies the range the |error| must be within for the integral
+     *              term to take effect. Must be positive, or set to zero to
+     *              disable.
+     * @param FF    FeedForward term
+     * @param name  Name that should be shown on the dashboard
      */
     public PIDFConstants(double P, double I, double D, double IZone, double FF, String name) {
         kP = P;
@@ -68,8 +79,10 @@ public class PIDFConstants implements Sendable {
 
     /**
      * Construct PIDF constants from a SparkMaxPIDController
-     * @param basePID {@link SparkMaxPIDController} that we should inherit initial values from
-     * @param slotID PID Slot to read
+     * 
+     * @param basePID {@link SparkMaxPIDController} that we should inherit initial
+     *                values from
+     * @param slotID  PID Slot to read
      */
     public PIDFConstants(SparkMaxPIDController basePID) {
         kP = basePID.getP();
@@ -90,20 +103,74 @@ public class PIDFConstants implements Sendable {
         builder.addDoubleProperty("FF", this::getFF, this::setFF);
     }
 
+    /* ---------- Utilities ---------- */
+
     /**
-     * Update a SparkMaxPIDController with the values in this object. Useful for tuning from the dashboard. <p>
-     * <b>IMPORTANT:</b> These settings will not persist unless the {@link com.revrobotics.CANSparkMax#burnFlash() CANSparkMax.burnFlash()} method is called.
-     * @param toUpdate {@link SparkMaxPIDController} to update  
+     * Update a SparkMaxPIDController with the values in this object. Useful for
+     * tuning from the dashboard.
+     * <p>
+     * <b>IMPORTANT:</b> These settings will not persist unless the
+     * {@link com.revrobotics.CANSparkMax#burnFlash() CANSparkMax.burnFlash()}
+     * method is called.
+     * 
+     * @param toUpdate {@link SparkMaxPIDController} to update
      */
-    public synchronized void updateSparkMax(SparkMaxPIDController toUpdate) {
-        // Method is synchronized to prevent simultaneous updates
-        if (toUpdate.getP() != kP) toUpdate.setP(kP);
-        if (toUpdate.getI() != kI) toUpdate.setI(kI);
-        if (toUpdate.getD() != kD) toUpdate.setD(kD);
-        if (toUpdate.getIZone() != kIZone) toUpdate.setIZone(kIZone);
-        if (toUpdate.getFF() != kFF) toUpdate.setFF(kFF);
+    public void updateSparkMax(SparkMaxPIDController toUpdate) {
+        // Method is synchronized to prevent simultaneous updates to the same PIDF
+        // object
+        synchronized (toUpdate) {
+            if (toUpdate.getP() != kP)
+                toUpdate.setP(kP);
+            if (toUpdate.getI() != kI)
+                toUpdate.setI(kI);
+            if (toUpdate.getD() != kD)
+                toUpdate.setD(kD);
+            if (toUpdate.getIZone() != kIZone)
+                toUpdate.setIZone(kIZone);
+            if (toUpdate.getFF() != kFF)
+                toUpdate.setFF(kFF);
+        }
     }
 
+    /**
+     * Update a WPILib PID Controller
+     * 
+     * @param toUpdate the PID Controller object to update
+     */
+    public void updatePID(PIDController toUpdate) {
+        // Synchronized to prevent updates to the same controller object
+        synchronized (toUpdate) {
+            synchronized (toUpdate) {
+                if (toUpdate.getP() != kP)
+                    toUpdate.setP(kP);
+                if (toUpdate.getI() != kI)
+                    toUpdate.setI(kI);
+                if (toUpdate.getD() != kD)
+                    toUpdate.setD(kD);
+            }
+        }
+    }
+
+    /**
+     * Update a WPILib Profiled PID Controller
+     * 
+     * @param toUpdate the Profiled PID Controller object to update
+     */
+    public void updatePID(ProfiledPIDController toUpdate) {
+        // Synchronized to prevent updates to the same controller object
+        synchronized (toUpdate) {
+            synchronized (toUpdate) {
+                if (toUpdate.getP() != kP)
+                    toUpdate.setP(kP);
+                if (toUpdate.getI() != kI)
+                    toUpdate.setI(kI);
+                if (toUpdate.getD() != kD)
+                    toUpdate.setD(kD);
+            }
+        }
+    }
+
+    /* ---------- Getters and Setters ---------- */
     public double getP() {
         return kP;
     }
