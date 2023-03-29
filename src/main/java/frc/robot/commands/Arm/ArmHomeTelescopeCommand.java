7c23feff60cf91d0b7b4c4ae941364ea761297fd
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-03-29 14:14:40 -0400

    update home telescope command

--- src/main/java/frc/robot/commands/Arm/ArmHomeTelescopeCommand.java
+++ src/main/java/frc/robot/commands/Arm/ArmHomeTelescopeCommand.java
@@ -5,13 +5,20 @@ import frc.robot.subsystems.ArmSubsystem;
 
 public class ArmHomeTelescopeCommand extends InstantCommand {
   private final ArmSubsystem arm = ArmSubsystem.getInstance();
+  boolean isRetracted;
 
-  public ArmHomeTelescopeCommand() {
+  /**
+   * @param retracted If true, set the soft-limit for the telescope fully
+   *                  retracted. If false, set the soft limit for the telescope
+   *                  fully extended.
+   */
+  public ArmHomeTelescopeCommand(boolean retracted) {
     addRequirements(arm);
+    isRetracted = retracted;
   }
 
   @Override
   public void initialize() {
-    arm.homeTelescope();
+    arm.homeTelescope(isRetracted);
   }
 }
