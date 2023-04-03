7121a692e800a50f92c18347bf25517dabf8d1b4
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-03 13:13:24 -0400

    merge in doublesolenoid constants

---
+++ src/main/java/frc/robot/utils/DoubleSolenoidConstants.java
@@ -0,0 +1,13 @@
+package frc.robot.utils;
+
+import lombok.Getter;
+
+public class DoubleSolenoidConstants {
+    @Getter
+    private final int forwardChannel, reverseChannel;
+
+    public DoubleSolenoidConstants(int forwardChannel, int reverseChannel) {
+        this.forwardChannel = forwardChannel;
+        this.reverseChannel = reverseChannel;
+    }
+}
