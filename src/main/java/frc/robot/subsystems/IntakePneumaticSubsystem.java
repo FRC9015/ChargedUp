e5118ec5e889e84d7dd0f301e7cd537078c8c3cd
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2023-04-10 18:39:26 -0400

    test intake detection speed thing

--- src/main/java/frc/robot/subsystems/IntakePneumaticSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakePneumaticSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -23,17 +24,29 @@ public class IntakePneumaticSubsystem extends SubsystemBase {
 
     CANSparkMax intakeMotor;
     DoubleSolenoid intake;
+    MedianFilter intakeCurrentFilter;
+    double intakeCurrent;
 
     private IntakePneumaticSubsystem() {
         intakeMotor = new CANSparkMax(IntakeConstants.INTAKE_DRIVE_CAN_ID, MotorType.kBrushless);
         intakeMotor.setSmartCurrentLimit(20, 30);
         intake = PneumaticHubSubsystem.getDoubleSolenoid(IntakeConstants.OPEN_CLOSE_SOLENOID);
+        intakeCurrentFilter = new MedianFilter(25);
         forward = false;
     }
 
     @Override
     public void periodic() {
         RobotState.setIntakeOpen(!(intake.get() == DoubleSolenoid.Value.kReverse));
+        intakeCurrent = intakeCurrentFilter.calculate(intakeMotor.getOutputCurrent());
+
+        if (Math.abs(intakeMotor.get()) > 0) {
+            if (intakeCurrent > 15) {
+                System.out.println("INTAKE CUBE IN");
+            } else {
+                System.out.println("INTAKE CUBE OUT");
+            }
+        }
     }
 
     public void openIntake() {
@@ -45,7 +58,7 @@ public class IntakePneumaticSubsystem extends SubsystemBase {
     }
 
     public void setIntakeMotorSpeed(double speed) {
-        intakeMotor.set(speed);
+        intakeMotor.set(intakeCurrent > 15 ? speed * 0.5 : speed);
     }
 
     public void switchIntake() {
