81094aa05d83faa5e1069a06622a22f60d576707
Raunak <69701767+REVSIX@users.noreply.github.com>
2023-09-15 19:17:48 -0400

    Created a new subsystem file for the new feeder intake.

---
+++ src/main/java/frc/robot/subsystems/FeederIntakeSubsystem.java
@@ -0,0 +1,62 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.RobotState;
+
+public class FeederIntakeSubsystem extends SubsystemBase {
+
+    private static FeederIntakeSubsystem INSTANCE;
+
+    @SuppressWarnings("WeakerAccess")
+    
+    public static FeederIntakeSubsystem getInstance() {
+        if (INSTANCE == null) INSTANCE = new FeederIntakeSubsystem();
+        return INSTANCE;
+    }
+
+    private boolean forward;
+
+    CANSparkMax feederIntakeMotor;
+    DoubleSolenoid feederIntake;
+
+    private FeederIntakeSubsystem() {
+        feederIntakeMotor = new CANSparkMax(IntakeConstants.INTAKE_DRIVE_CAN_ID, MotorType.kBrushless);
+        feederIntakeMotor.setSmartCurrentLimit(20, 30);
+        feederIntake = PneumaticHubSubsystem.getDoubleSolenoid(IntakeConstants.OPEN_CLOSE_SOLENOID);
+        forward = false;
+    }
+
+    @Override
+    public void periodic() {
+        RobotState.setIntakeOpen(!(feederIntake.get() == DoubleSolenoid.Value.kReverse));
+    }
+
+    public void extendFeeder() {
+        feederIntake.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void retractFeeder() {
+        feederIntake.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void setIntakeMotorSpeed(double speed) {
+        feederIntakeMotor.set(speed);
+    }
+
+    public void switchFeeder() {
+        if (forward) {
+            feederIntake.set(DoubleSolenoid.Value.kReverse);
+            forward = false;
+
+        } else {
+            feederIntake.set(DoubleSolenoid.Value.kForward);
+            forward = true;
+        }
+    }
+}
