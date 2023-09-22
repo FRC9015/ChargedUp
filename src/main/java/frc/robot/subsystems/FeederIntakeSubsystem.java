f339d5fe1d825560b5cfd9193cbd0982f8d3d2a8
Raunak <69701767+REVSIX@users.noreply.github.com>
2023-09-22 18:14:13 -0400

    Uptd. FeederIntakeSubsystem for the new constants

--- src/main/java/frc/robot/subsystems/FeederIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/FeederIntakeSubsystem.java
@@ -6,7 +6,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.FeederIntakeConstants;
 import frc.robot.RobotState;
 
 public class FeederIntakeSubsystem extends SubsystemBase {
@@ -14,7 +14,6 @@ public class FeederIntakeSubsystem extends SubsystemBase {
     private static FeederIntakeSubsystem INSTANCE;
 
     @SuppressWarnings("WeakerAccess")
-    
     public static FeederIntakeSubsystem getInstance() {
         if (INSTANCE == null) INSTANCE = new FeederIntakeSubsystem();
         return INSTANCE;
@@ -26,9 +25,9 @@ public class FeederIntakeSubsystem extends SubsystemBase {
     DoubleSolenoid feederIntake;
 
     private FeederIntakeSubsystem() {
-        feederIntakeMotor = new CANSparkMax(IntakeConstants.INTAKE_DRIVE_CAN_ID, MotorType.kBrushless);
+        feederIntakeMotor = new CANSparkMax(FeederIntakeConstants.FEEDER_INTAKE_DRIVE_CAN_ID, MotorType.kBrushless);
         feederIntakeMotor.setSmartCurrentLimit(20, 30);
-        feederIntake = PneumaticHubSubsystem.getDoubleSolenoid(IntakeConstants.OPEN_CLOSE_SOLENOID);
+        feederIntake = PneumaticHubSubsystem.getDoubleSolenoid(FeederIntakeConstants.OPEN_CLOSE_SOLENOID);
         forward = false;
     }
 
