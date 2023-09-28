fc90f90a72a1d4958eca9ee190f679c59ac51822
M1cahg <goodmanmicah1@gmail.com>
2023-09-28 16:52:27 -0400

    Updated CAN ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,7 +111,7 @@ public final class Constants {
 
     public static final class FeederIntakeConstants {
         public static final int FEEDER_INTAKE_DRIVE_CAN_ID =
-                87; // These constants are placeholder values TODO FIND THE CANSPARKMAX ID AND PUT
+                23; // These constants are placeholder values TODO FIND THE CANSPARKMAX ID AND PUT
         // IT HERE
 
         public static final DoubleSolenoidConstants OPEN_CLOSE_SOLENOID =
